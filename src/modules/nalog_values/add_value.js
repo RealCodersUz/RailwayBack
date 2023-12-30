966aecf49692bd152a6f9a48f08631edb6fc0dab
Muhammadjon Abduvahobov <115058559+Megacoderuzb@users.noreply.github.com>
2023-12-30 17:57:06 +0500

    nalog

--- src/modules/nalog_values/add_value.js
+++ src/modules/nalog_values/add_value.js
@@ -3,6 +3,7 @@ const Saldo = require("./Saldo");
 const Archives = require("../archives/Archives");
 const XLSX = require("xlsx");
 const fs = require("fs");
+const { BadRequestError } = require("../../shared/errors");
 // const { json } = require("body-parser");
 
 async function addNalog(data, user) {
@@ -29,75 +30,48 @@ async function addNalog(data, user) {
     console.log("Previous Month Number:", previousMonthData.number);
   } else {
     console.log("Bunday oy topilmadi yoki u birinchi oy");
+    return new BadRequestError("Ошибка ввода!!!");
   }
-  const wb = XLSX.utils.book_new();
-  const wsName = "Sheet1";
-  const ws = XLSX.utils.json_to_sheet(data.file);
-  XLSX.utils.book_append_sheet(wb, ws, wsName);
-
-  const excelBuffer = XLSX.write(wb, {
-    bookType: "xlsx",
-    type: "buffer",
-    mimeType:
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  });
-
-  fs.writeFileSync(
-    "public/" +
-      user.branch_name +
-      "_" +
-      data.month +
-      "_" +
-      data.year +
-      "_nalog.xlsx",
-    // wb,
-    excelBuffer
-  );
-
-  console.log("Excel fayli generatsiya qilindi: ${}.xlsx");
-  // try {
-  let saldoData = await Saldo.find({
-    branch_name: user.branch_name,
-    date: "01." + targetMonthData.number + "." + data.year,
-    is_deleted: false,
-  });
-  console.log(saldoData);
-  // } catch (error) {}
-  // let query = await AdmData.find({ month: data.month, year: data.year });
-  // console.log(query);
-  // if (query[0]) {
-  //   console.log(query[0].Nalog, "ifga kirdi");
-  //   let updatedNalog = query[0].Nalog.map(
-  //     (value, index) => value + data.Nalog[index]
-  //   );
-  //   console.log(updatedNalog, "updated");
-  //   let res = await AdmData.findByIdAndUpdate(
-  //     query[0]._id,
-  //     { Nalog: updatedNalog },
-  //     { new: true } // To get the updated document after the update
-  //   );
-
-  //   console.log(res, "bu res");
-  // } else {
-  //   console.log(data.Nalog);
-  //   let newDocument = await AdmData.create({
-  //     month: data.month,
-  //     year: data.year,
-  //     Nalog: data.Nalog,
-  //   });
-
-  //   // let res = await newDocument.save();
-  //   console.log(newDocument);
-  // }
   try {
+    const wb = XLSX.utils.book_new();
+    const wsName = "Sheet1";
+    const ws = XLSX.utils.json_to_sheet(data.file);
+    XLSX.utils.book_append_sheet(wb, ws, wsName);
+    const excelBuffer = XLSX.write(wb, {
+      bookType: "xlsx",
+      type: "buffer",
+      mimeType:
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+
+    fs.writeFileSync(
+      "public/" +
+        user.branch_name +
+        "_" +
+        data.month +
+        "_" +
+        data.year +
+        "_nalog.xlsx",
+      // wb,
+      excelBuffer
+    );
+
+    console.log("Excel fayli generatsiya qilindi: ${}.xlsx");
+    // try {
+    let saldoData = await Saldo.find({
+      branch_name: user.branch_name,
+      date: "01." + targetMonthData.number + "." + data.year,
+      is_deleted: false,
+    });
+    console.log(saldoData);
+
     console.log(user, "user");
     let newSaldo = await Saldo.create({
       date: "01." + targetMonthData.number + "." + data.year,
       branch_name: user.branch_name,
-      values: data.values,
-      // file:
-      //   user.branch_name + "_" + data.month + "_" + data.year + "_nalog.xlsx",
-      type: "Налог",
+      debit: data.debit,
+      kredit: data.kredit,
+      // type: "Налог",
     });
     console.log(newSaldo);
     let archive = Archives.create({
@@ -119,10 +93,12 @@ async function addNalog(data, user) {
       console.log(result, "result");
       return result;
     }
-    return { status: 400, message: "Oldin bunaqasi bogan" };
+    // return { status: 400, message: "Oldin bunaqasi bogan" };
+    return new BadRequestError("Такая информация была ранее введена!!!");
   } catch (error) {
     console.error(error.message);
-    return { error: error.message };
+    return new BadRequestError("Ошибка ввода!!!");
+    // return { error: error.message };
   }
 }
 
