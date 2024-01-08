0e9bfcc583ae8a532afe35164b33194df8449a96
Muhammadjon Abduvahobov <115058559+Megacoderuzb@users.noreply.github.com>
2024-01-08 12:07:14 +0500

    nalog qoshish

--- src/modules/nalog_values/add_value.js
+++ src/modules/nalog_values/add_value.js
@@ -8,7 +8,7 @@ const { BadRequestError } = require("../../shared/errors");
 // const { json } = require("body-parser");
 
 async function addNalog(data, user) {
-  console.log(data, "DATA");
+  // console.log(data, "DATA");
   let dates = [
     { name: "кв-1", number: "1" },
     { name: "кв-2", number: "2" },
@@ -26,7 +26,7 @@ async function addNalog(data, user) {
   let targetMonthData = dates.find((month) => month.name === data.month);
   let targetIndex = dates.findIndex((month) => month.name === data.month);
 
-  console.log(targetIndex, "Bu index");
+  // console.log(targetIndex, "Bu index");
   try {
     const wb = XLSX.utils.book_new();
     const wsName = "Sheet1";
@@ -51,24 +51,9 @@ async function addNalog(data, user) {
       excelBuffer
     );
 
-    console.log("Excel fayli generatsiya qilindi: ${}.xlsx");
-    // try {
-    // let saldoData = await Saldo.find({
-    //   branch_name: user.branch_name,
-    //   date: "01." + targetMonthData.number + "." + data.year,
-    //   is_deleted: false,
-    // });
-    // console.log(saldoData);
+    console.log("Excel fayli generatsiya qilindi");
+    // if (user.role == "super_admin" && data.branch_name == "Общий") {
 
-    // console.log(user, "user");
-    // let newSaldo = await Saldo.create({
-    //   date: "01." + targetMonthData.number + "." + data.year,
-    //   branch_name: user.branch_name,
-    //   debit: data.debit,
-    //   kredit: data.kredit,
-    //   // type: "Налог",
-    // });
-    // console.log(newSaldo);
     let start;
     if (targetIndex !== -1 && targetIndex > 0) {
       let previousMonthData = dates[targetIndex - 1];
@@ -85,8 +70,22 @@ async function addNalog(data, user) {
         console.error(error);
       }
     } else {
-      console.log("Bunday oy topilmadi yoki u birinchi oy");
-      return new BadRequestError("Ошибка ввода!!!");
+      let previousMonthData = dates[3];
+      console.log("Previous Month Name:", previousMonthData.name);
+      console.log("Previous Month Number:", previousMonthData.number);
+      console.log(data.year - 1);
+      try {
+        start = await Saldo.find({
+          month: previousMonthData.name,
+          year: data.year - 1,
+          branch_name: user.branch_name,
+        });
+        console.log(start, "start");
+      } catch (error) {
+        console.error(error);
+        return new BadRequestError("Ошибка ввода!!!");
+      }
+      // console.log("Bunday oy topilmadi yoki u birinchi oy");
     }
     let archive = Archives.create({
       name:
@@ -118,9 +117,9 @@ async function addNalog(data, user) {
         start[0].values.kredit.length > 0
       );
       if (
-        start[0] &&
-        start[0].values.debit.length > 0 &&
-        start[0].values.kredit.length > 0
+        start[0] &
+        (start[0].values.debit.length > 0) &
+        (start[0].values.kredit.length > 0)
       ) {
         for (let i = 0; i < start[0].values.debit.length; i++) {
           const element = start[0].values.debit[i];
@@ -161,6 +160,49 @@ async function addNalog(data, user) {
         final,
       });
       console.log(result, "result");
+      let query = await Nalog.findOne({
+        month: data.month,
+        year: data.year,
+        branch_name: "Общий",
+      });
+      if (query) {
+        let updatedValues = {
+          debit: [],
+          kredit: [],
+        };
+        updatedValues.debit = query[0].values.debit.map(
+          (value, index) => value + data.values.debit[index]
+        );
+        updatedValues.kredit = query[0].values.kredit.map(
+          (value, index) => value + data.values.kredit[index]
+        );
+        let updatedStart = query[0].start.map(
+          (value, index) => value + data.values[index]
+        );
+        let updatedFinal = query[0].values.map(
+          (value, index) => value + data.values[index]
+        );
+        console.log(updatedValues);
+        console.log(updatedStart);
+        console.log(updatedFinal);
+        let res = await Nalog.findByIdAndUpdate(
+          query[0]._id,
+          { values: updatedValues },
+          { start: updatedStart },
+          { final: updatedFinal },
+          { new: true }
+        );
+      } else {
+        await Nalog.create({
+          year: data.year,
+          month: data.month,
+          branch_name: "Общий",
+          values: data.values,
+          start: start[0].values,
+          final,
+        });
+      }
+      // }
       return result;
     }
     // return { status: 400, message: "Oldin bunaqasi bogan" };
